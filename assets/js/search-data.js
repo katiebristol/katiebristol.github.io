eff5cec479685ec6cfb3a1d555fa806ba4f57107
katiebristol <brist096@umn.edu>
2026-09-16 15:39:05 +0000

    Deploying to gh-pages from @ katiebristol/katiebristol.github.io@a7ab7b4a6dd325c4d8ba2e2332afbb663c001f2a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -61,6 +61,11 @@ ninja.data = [{
           description: "Field camp through University of Canterbury in Summer 2017.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/newzealand/";
+            },},{id: "projects-new-zealand",
+          title: 'New Zealand',
+          description: "Field camp through University of Canterbury in Summer 2017.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/test/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
